b2c94d89ed74ecd97ec1aad002e5205103c55bf9
Roo Kosherbay <kosher1@stolaf.edu>
2019-05-19 15:32:44 -0500

    update documentation

--- R/average_pred_without_KNN.R
+++ R/average_pred_without_KNN.R
@@ -1,23 +1,23 @@
-#' Ensemble of regression models
+#' Ensemble of regression models (the same as average_pred() but without KNN)
 #'
 #' When given training predictors, training responses, and predictors, this function
 #' outputs a vector of predictions
-#' The models used for this ensemble are: KNN, Random forest, LASSO, Ridge and linear
+#' The models used for this ensemble are: Random forest, LASSO, Ridge and linear
 #'
 #' @param X_train training predictor vector
 #' @param y_train training response vector
 #' @param X_test predictor vector
+#'
 #' @return A vector of predictions
 #' @export
-average_pred <- function(X_train, y_train, X_test){
+
+average_pred_without_KNN <- function(X_train, y_train, X_test){
   # Creating ridge and lasso sets
   X_train_glmnet <- model.matrix(~.-1, data=X_train)
   X_test_glmnet <- model.matrix(~.-1, data=X_test)
   # LM
   fit_lm <- glmnet(X_train_glmnet, y_train, alpha=0, lambda=c(0))
   pred_lm <- predict(fit_lm, X_test_glmnet)[,1]
-  # KNN
-  pred_knn <- knn.reg(X_train, test=X_test, y=y_train, k=5)$pred
   # RF
   fit_rf <- randomForest(x=X_train, y=y_train, ntree=100)
   pred_rf <- predict(fit_rf, X_test)
@@ -28,6 +28,6 @@ average_pred <- function(X_train, y_train, X_test){
   fit_lasso <- cv.glmnet(X_train_glmnet, y_train, alpha=1)
   pred_lasso <- predict(fit_lasso, X_test_glmnet, s='lambda.min')[,1]
   # Computing average
-  avg_preds <- rowMeans(matrix(c(pred_lm, pred_knn, pred_rf, pred_ridge, pred_lasso),ncol=5))
+  avg_preds <- rowMeans(matrix(c(pred_lm, pred_rf, pred_ridge, pred_lasso),ncol=4))
   return(avg_preds)
 }
