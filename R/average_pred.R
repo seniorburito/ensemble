8af4bb81adfffb849bfec22a1211ab1950bdf3a7
Roo Kosherbay <kosher1@stolaf.edu>
2019-05-20 09:55:45 -0500

    go back

--- R/average_pred.R
+++ R/average_pred.R
@@ -10,44 +10,67 @@
 #' @param rfntree number of trees to grow. This should not be set to too small a number
 #' @return A vector of predictions
 #' @export
-average_pred <- function(X_train, y_train, X_test, rfntree, KNNk, rfmtry, doKNN){
-  if(missing(rfntree)){
-    rfntree = 100
-  }
-  if(missing(KNNk)){
-    KNNk = 5
-  }
-  if(missing(rfmtry)){
-    rfmtry = 6
-  }
-  if(missing(doKNN)){
-    doKNN = TRUE
-  }
-  # Creating ridge and lasso sets
-  X_train_glmnet <- model.matrix(~.-1, data=X_train)
-  X_test_glmnet <- model.matrix(~.-1, data=X_test)
-  # LM
-  fit_lm <- glmnet(X_train_glmnet, y_train, alpha=0, lambda=c(0))
-  pred_lm <- predict(fit_lm, X_test_glmnet)[,1]
-  # KNN
-  pred_knn <- knn.reg(X_train, test=X_test, y=y_train, k=KNNk)$pred
-  # RF
-  fit_rf <- randomForest(x=X_train, y=y_train, ntree=rfntree, mtry = rfmtry)
-  pred_rf <- predict(fit_rf, X_test)
-  # Ridge
-  fit_ridge <- cv.glmnet(X_train_glmnet, y_train, alpha=0)
-  pred_ridge <- predict(fit_ridge, X_test_glmnet, s='lambda.min')[,1]
-  # LASSO
-  fit_lasso <- cv.glmnet(X_train_glmnet, y_train, alpha=1)
-  pred_lasso <- predict(fit_lasso, X_test_glmnet, s='lambda.min')[,1]
-  # Computing average
-  if(doKNN){
-    avg_preds <- rowMeans(matrix(c(pred_lm, pred_knn, pred_rf, pred_ridge, pred_lasso),ncol=5))
-    return(avg_preds)
-  }
-  else{
-    avg_preds <- rowMeans(matrix(c(pred_lm, pred_rf, pred_ridge, pred_lasso),ncol=4))
-    return(avg_preds)
-  }
-
+average_pred <- function(X_train, y_train, X_test){
+# Creating ridge and lasso sets
+X_train_glmnet <- model.matrix(~.-1, data=X_train)
+X_test_glmnet <- model.matrix(~.-1, data=X_test)
+# LM
+fit_lm <- glmnet(X_train_glmnet, y_train, alpha=0, lambda=c(0))
+pred_lm <- predict(fit_lm, X_test_glmnet)[,1]
+# KNN
+pred_knn <- knn.reg(X_train, test=X_test, y=y_train, k=5)$pred
+# RF
+fit_rf <- randomForest(x=X_train, y=y_train, ntree=100)
+pred_rf <- predict(fit_rf, X_test)
+# Ridge
+fit_ridge <- cv.glmnet(X_train_glmnet, y_train, alpha=0)
+pred_ridge <- predict(fit_ridge, X_test_glmnet, s='lambda.min')[,1]
+# LASSO
+fit_lasso <- cv.glmnet(X_train_glmnet, y_train, alpha=1)
+pred_lasso <- predict(fit_lasso, X_test_glmnet, s='lambda.min')[,1]
+# Computing average
+avg_preds <- rowMeans(matrix(c(pred_lm, pred_knn, pred_rf, pred_ridge, pred_lasso),ncol=5))
+return(avg_preds)
 }
+
+# average_pred <- function(X_train, y_train, X_test, rfntree, KNNk, rfmtry, doKNN){
+#   if(missing(rfntree)){
+#     rfntree = 100
+#   }
+#   if(missing(KNNk)){
+#     KNNk = 5
+#   }
+#   if(missing(rfmtry)){
+#     rfmtry = 6
+#   }
+#   if(missing(doKNN)){
+#     doKNN = TRUE
+#   }
+#   # Creating ridge and lasso sets
+#   X_train_glmnet <- model.matrix(~.-1, data=X_train)
+#   X_test_glmnet <- model.matrix(~.-1, data=X_test)
+#   # LM
+#   fit_lm <- glmnet(X_train_glmnet, y_train, alpha=0, lambda=c(0))
+#   pred_lm <- predict(fit_lm, X_test_glmnet)[,1]
+#   # KNN
+#   pred_knn <- knn.reg(X_train, test=X_test, y=y_train, k=KNNk)$pred
+#   # RF
+#   fit_rf <- randomForest(x=X_train, y=y_train, ntree=rfntree, mtry = rfmtry)
+#   pred_rf <- predict(fit_rf, X_test)
+#   # Ridge
+#   fit_ridge <- cv.glmnet(X_train_glmnet, y_train, alpha=0)
+#   pred_ridge <- predict(fit_ridge, X_test_glmnet, s='lambda.min')[,1]
+#   # LASSO
+#   fit_lasso <- cv.glmnet(X_train_glmnet, y_train, alpha=1)
+#   pred_lasso <- predict(fit_lasso, X_test_glmnet, s='lambda.min')[,1]
+#   # Computing average
+#   if(doKNN){
+#     avg_preds <- rowMeans(matrix(c(pred_lm, pred_knn, pred_rf, pred_ridge, pred_lasso),ncol=5))
+#     return(avg_preds)
+#   }
+#   else{
+#     avg_preds <- rowMeans(matrix(c(pred_lm, pred_rf, pred_ridge, pred_lasso),ncol=4))
+#     return(avg_preds)
+#   }
+# }
+
